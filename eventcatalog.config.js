cee3f28c2263b55d80506a3255b161db5035ce99
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2026-05-24 14:07:56 +0300

    Small changes to the configuration

--- eventcatalog.config.js
+++ eventcatalog.config.js
@@ -1,41 +1,52 @@
 /** @type {import('@eventcatalog/core/bin/eventcatalog.config').Config} */
 export default {
-  title: 'EventCatalog',
+  title: 'Pennysheet',
+
   tagline:
     'This internal platform provides a comprehensive view of our event-driven architecture across all systems. Use this portal to discover existing domains, explore services and their dependencies, and understand the message contracts that connect our infrastructure',
+
   organizationName: 'Pennysheet',
-  theme: 'sunset',
-  homepageLink: 'https://eventcatalog.dev/',
-  editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  theme: 'forest',
+  homepageLink: 'https://github.com/triluu03/pennysheet',
+  editUrl: 'https://github.com/triluu03/pennysheet-catalog',
+
   // Supports static or server. Static renders a static site, server renders a server side rendered site
   // large catalogs may benefit from server side rendering
   output: 'static',
+
   // By default set to false, add true to get urls ending in /
   trailingSlash: false,
+
   // Change to make the base url of the site different, by default https://{website}.com/docs,
   // changing to /company would be https://{website}.com/company/docs,
   base: '/pennysheet-catalog/',
+
   // Resource search is the default lightweight search. Change this to { type: 'indexed' }
   // to enable full-content search. Indexed search requires running a build to generate the index.
   search: {
     type: 'resource',
   },
+
   // Customize the navigation for your docs sidebar.
   // read more at https://eventcatalog.dev/docs/development/customization/customize-sidebars/documentation-sidebar
   navigation: {
     pages: ['list:all'],
   },
+
   // Customize the logo, add your logo to public/ folder
   logo: {
-    alt: 'EventCatalog Logo',
     src: '/logo.png',
-    text: 'EventCatalog',
+    text: 'Pennysheet'
   },
+
   // This lets you copy markdown contents from EventCatalog to your clipboard
   // Including schemas for your events and services
   llmsTxt: {
     enabled: true,
   },
+
   // required random generated id used by eventcatalog
   cId: '9c7bd5d1-e04f-4de3-b616-18f27364ce7c',
+
+  repositoryUrl: 'https://github.com/triluu03/pennysheet-catalog'
 };
